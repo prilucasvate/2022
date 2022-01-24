6743bd262dfde99d9b0d1e058b335bdb9d29e544
lucas <97732145+prilucasvate@users.noreply.github.com>
2022-01-24 17:11:53 +0800

    Add files via upload

---
+++ src/aaaself/Aaaself.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aaaself;
+
+import java.awt.Frame;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author User
+ */
+
+public class Aaaself {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        
+        // TODO code application logic here
+        
+        new welcvoc().setVisible(true);
+        }
+
+  
+
+    }
+
