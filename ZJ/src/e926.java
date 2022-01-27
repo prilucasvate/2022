eb47a39fbe70a258d3d833382cfc3471077a6f7a
lucas <97732145+prilucasvate@users.noreply.github.com>
2022-01-27 23:33:54 +0800

    Add files via upload

---
+++ ZJ/src/e926.java
@@ -0,0 +1,13 @@
+
+public class e926 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("|'o'|\r\n"
+				+ "\\^_^/\r\n"
+				+ "(\"o\")\r\n"
+				+ "[-%-]");
+
+	}
+
+}
